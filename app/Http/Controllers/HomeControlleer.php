18f5ab28846aa9821732e030b25bac52204ce3be
ricardo24si-jpg <ricardo24si@mahasiswa.pcr.ac.id>
2025-10-02 08:07:18 +0700

    kardo

--- app/Http/Controllers/HomeControlleer.php
+++ app/Http/Controllers/HomeControlleer.php
@@ -71,7 +71,7 @@ class HomeControlleer extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id )
     {
         //
     }
